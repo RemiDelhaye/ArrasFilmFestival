b6cda999db24e54d3c5cca18688f7703d6098ba5
Rémi Delhaye <rdelhaye@epsi.fr>
2013-03-19 13:20:03 +0100

    BackOffice - Templating, design and Updating
    Photo & Template

---
+++ app/cache/dev/twig/9a/53/9be50b92dbb50f508128d84659b8.php
@@ -0,0 +1,83 @@
+<?php
+
+/* WebProfilerBundle:Profiler:toolbar_js.html.twig */
+class __TwigTemplate_9a539be50b92dbb50f508128d84659b8 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div id=\"sfwdt";
+        echo twig_escape_filter($this->env, $this->getContext($context, "token"), "html", null, true);
+        echo "\" class=\"sf-toolbar\" style=\"display: none\"></div>
+";
+        // line 2
+        $this->env->loadTemplate("WebProfilerBundle:Profiler:base_js.html.twig")->display($context);
+        // line 3
+        echo "<script type=\"text/javascript\">/*<![CDATA[*/
+    (function () {
+        ";
+        // line 5
+        if (("top" == $this->getContext($context, "position"))) {
+            // line 6
+            echo "            var sfwdt = document.getElementById('sfwdt";
+            echo twig_escape_filter($this->env, $this->getContext($context, "token"), "html", null, true);
+            echo "');
+            document.body.insertBefore(
+                document.body.removeChild(sfwdt),
+                document.body.firstChild
+            );
+        ";
+        }
+        // line 12
+        echo "
+        Sfjs.load(
+            'sfwdt";
+        // line 14
+        echo twig_escape_filter($this->env, $this->getContext($context, "token"), "html", null, true);
+        echo "',
+            '";
+        // line 15
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_wdt", array("token" => $this->getContext($context, "token"))), "html", null, true);
+        echo "',
+            function(xhr, el) {
+                el.style.display = -1 !== xhr.responseText.indexOf('sf-toolbarreset') ? 'block' : 'none';
+            },
+            function(xhr) {
+                if (xhr.status !== 0) {
+                    confirm('An error occurred while loading the web debug toolbar (' + xhr.status + ': ' + xhr.statusText + ').\\n\\nDo you want to open the profiler?') && (window.location = '";
+        // line 21
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_profiler", array("token" => $this->getContext($context, "token"))), "html", null, true);
+        echo "');
+                }
+            }
+        );
+    })();
+/*]]>*/</script>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "WebProfilerBundle:Profiler:toolbar_js.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  59 => 21,  50 => 15,  42 => 12,  32 => 6,  30 => 5,  26 => 3,  24 => 2,  19 => 1,  153 => 71,  148 => 70,  145 => 69,  140 => 61,  134 => 20,  130 => 19,  125 => 18,  122 => 17,  116 => 9,  106 => 73,  104 => 69,  95 => 62,  93 => 61,  74 => 45,  67 => 41,  46 => 14,  44 => 17,  33 => 9,  23 => 1,  107 => 43,  100 => 38,  88 => 32,  82 => 29,  75 => 25,  69 => 24,  65 => 23,  61 => 22,  55 => 21,  52 => 20,  48 => 19,  31 => 4,  28 => 3,);
+    }
+}

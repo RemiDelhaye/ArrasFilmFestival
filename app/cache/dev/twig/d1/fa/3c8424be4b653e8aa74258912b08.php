85db7c0074c9b926a7949900b1178781532b1902
Thomas Leduc <tleduc2@hotmail.fr>
2013-03-19 15:47:40 +0100

    Podcast upload finish.
    Function upload work.
    
    Signed-off-by: Thomas Leduc <tleduc2@hotmail.fr>

--- app/cache/dev/twig/d1/fa/3c8424be4b653e8aa74258912b08.php
+++ app/cache/dev/twig/d1/fa/3c8424be4b653e8aa74258912b08.php
@@ -50,10 +50,14 @@ class __TwigTemplate_d1fa3c8424be4b653e8aa74258912b08 extends Twig_Template
         </tr>
         <tr>
             <th>Path</th>
-            <td>";
-        // line 23
+            <td><audio controls>
+  <source src=\"";
+        // line 24
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("uploads/audios/"), "html", null, true);
         echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "path"), "html", null, true);
-        echo "</td>
+        echo "\" type=\"audio/ogg\">
+Your browser does not support the audio element.
+</audio><audio></audio></td>
         </tr>
     </tbody>
 </table>
@@ -61,7 +65,7 @@ class __TwigTemplate_d1fa3c8424be4b653e8aa74258912b08 extends Twig_Template
 <ul class=\"record_actions\">
     <li>
         <a href=\"";
-        // line 30
+        // line 33
         echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("podcast"), "html", null, true);
         echo "\">
             Back to the list
@@ -69,7 +73,7 @@ class __TwigTemplate_d1fa3c8424be4b653e8aa74258912b08 extends Twig_Template
     </li>
     <li>
         <a href=\"";
-        // line 35
+        // line 38
         echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("podcast_edit", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"))), "html", null, true);
         echo "\">
             Edit
@@ -77,11 +81,11 @@ class __TwigTemplate_d1fa3c8424be4b653e8aa74258912b08 extends Twig_Template
     </li>
     <li>
         <form action=\"";
-        // line 40
+        // line 43
         echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("podcast_delete", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"))), "html", null, true);
         echo "\" method=\"post\">
             ";
-        // line 41
+        // line 44
         echo $this->env->getExtension('form')->renderer->searchAndRenderBlock((isset($context["delete_form"]) ? $context["delete_form"] : $this->getContext($context, "delete_form")), 'widget');
         echo "
             <button type=\"submit\">Delete</button>
@@ -103,6 +107,6 @@ class __TwigTemplate_d1fa3c8424be4b653e8aa74258912b08 extends Twig_Template
 
     public function getDebugInfo()
     {
-        return array (  85 => 41,  81 => 40,  73 => 35,  65 => 30,  55 => 23,  48 => 19,  41 => 15,  34 => 11,  27 => 7,  19 => 1,);
+        return array (  89 => 44,  85 => 43,  77 => 38,  69 => 33,  56 => 24,  48 => 19,  41 => 15,  34 => 11,  27 => 7,  19 => 1,);
     }
 }
